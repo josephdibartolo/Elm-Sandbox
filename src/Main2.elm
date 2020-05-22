9630ad499099f5b2c040fa33bf2a20787a4560e3
Joseph DiBartolo <joseph.dibartolo@ibm.com>
2020-05-22 16:44:04 -0400

    the exploration begins

---
+++ src/Main2.elm
@@ -0,0 +1,65 @@
+module Main2 exposing (..)
+
+import Browser
+import Html exposing (Html, Attribute, div, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { content : String
+  }
+
+
+init : Model
+init =
+  { content = "" }
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Change String
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Change newContent ->
+      { model | content = newContent }
+
+
+
+-- VIEW
+formatLength : String -> String
+formatLength content = 
+    let
+        contentLength = String.length content
+    in
+    if contentLength == 0 then
+        "No string here."
+    else
+        String.fromInt contentLength
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ input [ placeholder "Text to reverse", value model.content, onInput Change ] []
+    , div [] [ text (String.reverse model.content) ]
+    , div [] [ text (formatLength model.content) ]
+    ]
